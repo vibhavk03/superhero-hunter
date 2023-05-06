c746530776a07660ea69c1fd455084b8d528fa29
vibhavk03 <vibhavkashyap3@gmail.com>
2023-05-06 13:45:45 +0530

    adding favourites local styling

--- favourites.js
+++ favourites.js
@@ -1,1 +1,16 @@
 console.log("javascript loaded for favourites");
+
+const favouritesDisplayDiv = document.getElementById("favourites-display");
+
+let favouritesArray = JSON.parse(localStorage.getItem("favouriteHeros"));
+
+const removeFromFavourites = () => {
+  console.log("hehehehehehhehehehe");
+};
+
+if (!favouritesArray) {
+  console.log("no favourites");
+  favouritesDisplayDiv.innerHTML = "<h2>No Favourites Added yet!</h2>";
+} else {
+  console.log("favourites");
+}
